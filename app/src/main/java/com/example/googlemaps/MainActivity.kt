2a3584ccaf0879acddb20111b1e3f71e1f6cc1fc
Andrij Demchuk <stazi@ukr.net>
2023-12-10 14:54:07 +0200

    map

--- app/src/main/java/com/example/googlemaps/MainActivity.kt
+++ app/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.googlemaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun OnMapReady(googleMap: GoogleMap){
+        mMap = googleMap
+        var point = LatLng(50.718531796658844, 25.313874166220373)
+        mMap.addMarker(MarkerOptions().position(point).title("Я"))
+        mMap.isBuildingsEnabled = true
+        mMap.isIndoorEnabled = true
+
     }
 }
