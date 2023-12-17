492037d9d16950c07480b302f9591800a3e77cae
Andrij Demchuk <stazi@ukr.net>
2023-12-17 14:05:21 +0200

    goloc1

--- app/src/main/java/com/example/googlemaps/MainActivity.kt
+++ app/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.googlemaps
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import com.google.android.gms.common.api.GoogleApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.provider.Settings
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import java.util.Locale
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    var latitude = 60.718531796658844
+    var longitude = 45.313874166220373
+    private val permissionId = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocation()
+
         var mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
@@ -22,10 +50,70 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap){
         mMap = googleMap
-        var point = LatLng(50.718531796658844, 25.313874166220373)
-        mMap.addMarker(MarkerOptions().position(point).title("Тут був я"))
+        var point = LatLng(latitude, longitude)
+
+        mMap.addMarker(MarkerOptions().position(point).title("Я тут"))
         mMap.isBuildingsEnabled = true
         mMap.isIndoorEnabled = true
         mMap.moveCamera(CameraUpdateFactory.newLatLng(point))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 15f))
     }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLocation( ) {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: List<Address>? =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                        latitude = list?.get(0)!!.latitude
+                        longitude = list?.get(0)!!.longitude
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Увімкніть геолокацію", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else { requestPermissions() }
+    }
+
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+            )
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+           if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            }
+            return false
+    }
+
 }
